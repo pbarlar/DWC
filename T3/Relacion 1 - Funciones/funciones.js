5b9fd62cd0e7b9067ff223753cdb356673b6a711
pbarlar <pbarlar@g.educaand.es>
2025-10-04 23:10:13 +0200

    Add exercises 4-7 and related functions
    Added HTML files for exercises 4 to 7, each demonstrating a specific mathematical function. Implemented corresponding functions in funciones.js: volumenEsfera, areaCirculo, potencia, and factorial.

--- T3/Relacion 1 - Funciones/funciones.js
+++ T3/Relacion 1 - Funciones/funciones.js
@@ -10,11 +10,59 @@ function maxValue(num1, num2, num3, num4) {
     return Math.max(num1, num2, num3, num4);
 }
 
-/**
+/**Ejercicio 2 y 3
  * 
  * @returns Devuelve un número aleatorio entre 1 y 6
  */
 
 function lanzamiento() {
     return Math.floor(Math.random()*6)+1;
+}
+
+
+/**
+ * Ejercicio 4
+ * @param {*} radio Radio de esfera
+ * @returns Volumen de la esfera
+ */
+function volumenEsfera(radio) {
+    return ((4/3)*Math.PI*Math.pow(radio,3));
+}
+
+/**
+ * Ejercicio 5
+ * @param {*} radio Radio del circulo
+ * @returns Area del circulo
+ */
+function areaCirculo(radio) {
+    return Math.PI*(radio*radio);
+}
+
+
+/**Ejercicio 6
+ * Funcion para calcular la potencia de cualquier número
+ * @param {*} num Base 
+ * @param {*} exponente Exponente
+ * @returns Potencia de la base elevado al exponente
+ */
+function potencia(base,exponente) {
+    if (exponente==0) {
+        return 1;
+    }else{
+        return potencia(base,exponente-1)*base;
+    }
+}
+
+
+/**Ejercicio 7
+ * Función que calcula factorial de un número
+ * @param {*} num1 
+ * @returns Devuelve el factorial del número introducido
+ */
+function factorial(num1) {
+    if (num1<2) {
+        return 1;
+    }else{
+        return factorial(num1-1)*num1;
+    }
 }
