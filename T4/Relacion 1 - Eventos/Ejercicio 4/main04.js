e440eb2cc40823c1c2e4b6282b0acc3adc3a7c9b
pbarlar <pbarlar@g.educaand.es>
2025-11-07 10:24:00 +0100

    Add grid layout and mouseover event script
    Created index.html with a 3x3 grid layout and main04.js to handle mouseover events. The script is intended to change cell color when Shift is pressed, but currently contains a bug referencing an undefined variable 'i'.

---
+++ T4/Relacion 1 - Eventos/Ejercicio 4/main04.js
@@ -0,0 +1,16 @@
+function pintar() {
+    let celdas = document.querySelectorAll(".padre div");
+
+    document.addEventListener("mouseover",(event)=>{
+        if (event.shiftKey) {
+            celdas[i].style.backgroundColor="blue";
+        }
+    })
+}
+
+
+
+
+(()=>{
+
+})();
