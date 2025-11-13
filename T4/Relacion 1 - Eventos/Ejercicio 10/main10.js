efc47665729c4dfe6f3eb139cd832f151c557b11
pbarlar <pbarlar@g.educaand.es>
2025-11-13 18:04:19 +0100

    Ejercicio 10 arrastrar imagenes terminado

---
+++ T4/Relacion 1 - Eventos/Ejercicio 10/main10.js
@@ -0,0 +1,40 @@
+/**
+ * Función que permite arrastrar la imagen mientras mantienes pulsado el botón del ratón
+ */
+function moverImagenes() {
+  const imagenes = document.getElementsByClassName("imagen");
+
+  Array.from(imagenes).forEach((imagen) => {
+    imagen.style.position = "absolute";
+    imagen.style.cursor = "grab";
+
+    let moviendo = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    imagen.addEventListener("mousedown", (event) => {
+      moviendo = true;
+      imagen.style.cursor = "grabbing";
+
+      // Calculamos la distancia entre el cursor y la posición actual de la imagen
+      offsetX = event.clientX - imagen.offsetLeft;
+      offsetY = event.clientY - imagen.offsetTop;
+    });
+
+    document.addEventListener("mouseup", () => {
+      moviendo = false;
+      imagen.style.cursor = "grab";
+    });
+
+    document.addEventListener("mousemove", (event) => {
+      if (moviendo) {
+        imagen.style.left = event.clientX - offsetX + "px";
+        imagen.style.top = event.clientY - offsetY + "px";
+      }
+    });
+  });
+}
+
+(() => {
+  moverImagenes();
+})();
