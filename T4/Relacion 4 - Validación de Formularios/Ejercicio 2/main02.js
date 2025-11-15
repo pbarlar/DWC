6deed900c5b2265d9d0757a8b6066447ebcbc53b
pbarlar <pbarlar@g.educaand.es>
2025-11-15 15:54:28 +0100

    Update main02.js

--- T4/Relacion 4 - Validación de Formularios/Ejercicio 2/main02.js
+++ T4/Relacion 4 - Validación de Formularios/Ejercicio 2/main02.js
@@ -13,21 +13,24 @@ function comprobacionRegistro() {
 
     name.addEventListener("blur",()=>{
       if (!expNombre.test(name.value)) {
-        document.getElementById("nombre").innerHTML+="(Formato invalido)"
+        document.getElementById("nombre").innerHTML+="<br>(Debe empezar por mayuscula)"
+        document.getElementById("nombre").style.color="red";
+      }else{
+        document.getElementById("nombre").innerHTML+="<br>(Debe empezar por mayuscula)"
         document.getElementById("nombre").style.color="red";
       }  
     })
 
     dni.addEventListener("blur",()=>{
       if (!expDni.test(dni.value)) {
-        document.getElementById("lbl_dni").innerHTML+="(Formato invalido)"
+        document.getElementById("lbl_dni").innerHTML+="<br>(8 numeros y letra mayuscula)"
         document.getElementById("lbl_dni").style.color="red";
       }  
     })
 
     tel.addEventListener("blur",()=>{
       if (!expTel.test(tel.value)) {
-        document.getElementById("lbl_tel").innerHTML+="(Formato invalido)"
+        document.getElementById("lbl_tel").innerHTML+="<br>(9 numeros)"
         document.getElementById("lbl_tel").style.color="red";
       }  
     })
